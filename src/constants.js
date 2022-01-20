730024f2ce0855f2a71d55f7308592dc910e2f5b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-20 10:11:46 +0530

    updated data url v1

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const GET_DATA_URL =
-  "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+  'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
